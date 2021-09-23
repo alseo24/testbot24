7a39377706a4a8306cacec9af531146c4d823471
Al Seo <91246405+alseo24@users.noreply.github.com>
2021-09-23 13:16:32 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,4 +13,7 @@ client = discord.Client()
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
+print('*** Bot started ***')
 client.run('ODkwNTExNDAwMDQ3MzUzODU2.YUw3Xg.AfW58hQ-5DKNVmyVMaBAgQv0IRc')
+print('*** Bot stopped ***')
+
